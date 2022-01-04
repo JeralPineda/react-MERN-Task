7c462f727e6daa52f8e7fd392037fd4f0721a8b3
Jeral <jeral.cris@gmail.com>
2022-01-04 09:45:30 -0600

    Creando componentes y routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import Projects from './components/projects/Projects';
+
 function App() {
    return (
-      <div>
-         <h1>MERN Task</h1>
-      </div>
+      <Router>
+         <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/nueva-cuenta" element={<Signup />} />
+            <Route path="/proyectos" element={<Projects />} />
+         </Routes>
+      </Router>
    );
 }
 

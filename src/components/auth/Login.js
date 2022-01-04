29a41ea214eeeb2ca4e560b09bcc6d094a10a182
Jeral <jeral.cris@gmail.com>
2022-01-04 11:42:39 -0600

    leyendo la info del formulario y guardandola en el state

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+   // State para iniciar sesión
+   const [usuario, setUsuario] = useState({
+      email: '',
+      password: '',
+   });
+
+   // Extraer usuario
+   const { email, password } = usuario;
+
+   const handleChange = (e) => {
+      setUsuario({
+         ...usuario,
+         [e.target.name]: e.target.value,
+      });
+   };
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+
+      //Validar el formulario
+
+      //Pasar al action
+   };
+
    return (
-      <div>
-         <h1>Login</h1>
+      <div className="form-usuario">
+         <div className="contenedor-form sombra-dark">
+            <h1>Iniciar Sesión</h1>
+
+            <form onSubmit={handleSubmit}>
+               <div className="campo-form">
+                  <label htmlFor="email">Email</label>
+                  <input
+                     //
+                     type="email"
+                     id="name"
+                     name="email"
+                     placeholder="Tu Email"
+                     value={email}
+                     onChange={handleChange}
+                  />
+               </div>
+
+               <div className="campo-form">
+                  <label htmlFor="email">Password</label>
+                  <input
+                     //
+                     type="password"
+                     id="password"
+                     name="password"
+                     placeholder="Tu Password"
+                     value={password}
+                     onChange={handleChange}
+                  />
+               </div>
+
+               <div className="campo-form">
+                  <button type="submit" className="btn btn-primario btn-block">
+                     Iniciar Sesión
+                  </button>
+               </div>
+            </form>
+
+            <Link to="/nueva-cuenta" className="enlace-cuenta">
+               Crear Cuenta
+            </Link>
+         </div>
       </div>
    );
 };

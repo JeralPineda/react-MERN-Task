670d350354d680043173bcbe4435c788de339c99
Jeral <jeral.cris@gmail.com>
2022-01-04 21:01:41 -0600

    Leyendo la informacion del formulario y usando state

--- src/components/projects/NewProject.js
+++ src/components/projects/NewProject.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import { useState } from 'react';
 
 const NewProject = () => {
+   // State del proyecto
+   const [proyecto, setProyecto] = useState({
+      nombre: '',
+   });
+
+   const handleChange = (e) => {
+      setProyecto({
+         ...proyecto,
+         [e.target.name]: e.target.value,
+      });
+   };
+
+   // Extraer el nombre del state
+   const { nombre } = proyecto;
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+
+      // Validar el proyecto
+
+      // Agregar al state
+
+      // Limpiar el formulario
+   };
+
    return (
       <>
          <button type="button" className="btn btn-block btn-primario">
             Nuevo Proyecto
          </button>
 
-         <form className="formulario-nuevo-proyecto">
+         <form
+            //
+            className="formulario-nuevo-proyecto"
+            onSubmit={handleSubmit}>
             <input
                //
                type="text"
                className="input-text"
                placeholder="Nombre Proyecto"
                name="nombre"
+               value={nombre}
+               onChange={handleChange}
             />
 
             <button type="submit" className="btn btn-block btn-primario">

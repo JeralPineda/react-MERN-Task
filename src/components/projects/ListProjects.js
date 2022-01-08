7babb14cca98facb8493de882d56770dda63888e
Jeral <jeral.cris@gmail.com>
2022-01-08 00:21:17 -0600

    desabilitando alerta de dependencia

--- src/components/projects/ListProjects.js
+++ src/components/projects/ListProjects.js
@@ -13,6 +13,7 @@ const ListProjects = () => {
    // Obtener proyectos cuando carga el componente
    useEffect(() => {
       obtenerProyectos();
+      // eslint-disable-next-line
    }, []);
 
    if (proyectos.length === 0) return <p>No hay proyectos, comienza creando uno</p>;

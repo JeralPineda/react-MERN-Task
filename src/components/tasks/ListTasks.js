1c43fdf945e4b1cd25f1d238165a126791fdf8bb
Jeral <jeral.cris@gmail.com>
2022-01-07 22:00:03 -0600

    Agregando una animacion cuando se crean nuevas tareas

--- src/components/tasks/ListTasks.js
+++ src/components/tasks/ListTasks.js
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import Task from './Task';
+
 import proyectoContext from '../../context/projects/projectContext';
 import TareaContext from '../../context/tasks/tareaContext';
 
@@ -33,13 +35,17 @@ const ListTasks = () => {
                   <p>No hay tareas</p>
                </li>
             ) : (
-               tareasproyecto.map((tarea) => (
-                  <Task
-                     //
-                     key={tarea.id}
-                     tarea={tarea}
-                  />
-               ))
+               <TransitionGroup>
+                  {tareasproyecto.map((tarea) => (
+                     <CSSTransition
+                        //
+                        key={tarea.id}
+                        timeout={200}
+                        classNames="tarea">
+                        <Task tarea={tarea} />
+                     </CSSTransition>
+                  ))}
+               </TransitionGroup>
             )}
          </ul>
 

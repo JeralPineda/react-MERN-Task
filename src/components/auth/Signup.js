5715e6d00046ae51b9adf64a0dcf76f6a4566d9b
Jeral <jeral.cris@gmail.com>
2022-01-15 16:14:50 -0600

    Eliminando botones de github y google en el componente registro

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -91,26 +91,6 @@ const Signup = () => {
                      Crear Cuenta
                   </button>
                </div>
-
-               <div className="row-btn">
-                  <div className="google-btn">
-                     <div className="google-icon-wrapper">
-                        <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
-                     </div>
-                     <p className="btn-text">
-                        <b>Crear con Google</b>
-                     </p>
-                  </div>
-
-                  <div className="google-btn">
-                     <div className="google-icon-wrapper">
-                        <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Font_Awesome_5_brands_github.svg/640px-Font_Awesome_5_brands_github.svg.png" alt="google button" />
-                     </div>
-                     <p className="btn-text">
-                        <b>Crear con GitHub</b>
-                     </p>
-                  </div>
-               </div>
             </form>
 
             <Link to="/" className="enlace-cuenta">

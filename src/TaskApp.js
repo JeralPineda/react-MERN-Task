c576751fae1b6535976cdc31b05ad2f0a620e7dc
Jeral <jeral.cris@gmail.com>
2022-01-21 20:17:33 -0600

    Limpiando importaciones si uso

--- src/TaskApp.js
+++ src/TaskApp.js
@@ -1,8 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/auth/Login';
-import Signup from './components/auth/Signup';
-import Projects from './components/projects/Projects';
-
 import ProyectoState from './context/projects/projectState';
 import TareaState from './context/tasks/tareaState';
 import AlertaState from './context/alerts/alertState';

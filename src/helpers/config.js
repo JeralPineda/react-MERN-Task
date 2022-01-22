7cabb7700262b904862f090aa229b8a2bf0097b6
Jeral <jeral.cris@gmail.com>
2022-01-21 18:49:29 -0600

    Archivo config para las variables de entorno

---
+++ src/helpers/config.js
@@ -0,0 +1,4 @@
+export const clientId = process.env.REACT_APP_CLIENT_ID_GOOGLE;
+export const clientIdGitHub = process.env.REACT_APP_GITHUB_CLIENT_ID;
+export const authorizeUri = process.env.REACT_APP_GITHUB_AUTORIZE_URI;
+export const redirectUri = process.env.REACT_APP_GITHUB_REDIRECT_URI;

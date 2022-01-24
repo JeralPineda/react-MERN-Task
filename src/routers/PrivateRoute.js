114edcea32ea55a60a01537aee0cedea80ebf597
Jeral <jeral.cris@gmail.com>
2022-01-23 21:50:14 -0600

    agregando ruta privada para la app

---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import AuthContext from '../context/auth/authContext';
+
+export const PrivateRoute = ({ children }) => {
+   const authContext = useContext(AuthContext);
+   const { autenticado } = authContext;
+
+   return autenticado ? children : <Navigate to="/" />;
+};

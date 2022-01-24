c6923adba27cede2999b0709b9e3a30d0525e7aa
Jeral <jeral.cris@gmail.com>
2022-01-23 21:44:06 -0600

    Creando pagina 404

---
+++ src/components/pages/Page404.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Page404.css';
+
+export const Page404 = () => {
+   return (
+      <figure>
+         <div className="sad-mac"></div>
+         <figcaption>
+            <span className="sr-text">Error 404: Not Found</span>
+            <span className="e"></span>
+            <span className="r"></span>
+            <span className="r"></span>
+            <span className="o"></span>
+            <span className="r"></span>
+            <span className="_4"></span>
+            <span className="_0"></span>
+            <span className="_4"></span>
+            <span className="n"></span>
+            <span className="o"></span>
+            <span className="t"></span>
+            <span className="f"></span>
+            <span className="o"></span>
+            <span className="u"></span>
+            <span className="n"></span>
+            <span className="d"></span>
+         </figcaption>
+
+         <div className="button">
+            <Link to="/" className="return-home">
+               Volver a inicio
+            </Link>
+         </div>
+      </figure>
+   );
+};

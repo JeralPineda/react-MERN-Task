8315f7c0c1a143bb54d1065d10803a28067fb2d6
Jeral <jeral.cris@gmail.com>
2022-01-23 21:50:37 -0600

    agregando rutas publicas, privadas y 404

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,15 +2,43 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from '../components/auth/Login';
 import Signup from '../components/auth/Signup';
+import { Page404 } from '../components/pages/Page404';
 import Projects from '../components/projects/Projects';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 const AppRouter = () => {
    return (
       <Router>
          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/nueva-cuenta" element={<Signup />} />
-            <Route path="/proyectos" element={<Projects />} />
+            <Route path="*" element={<Page404 />} />
+
+            <Route
+               path="/"
+               element={
+                  <PublicRoute>
+                     <Login />
+                  </PublicRoute>
+               }
+            />
+
+            <Route
+               path="/nueva-cuenta"
+               element={
+                  <PublicRoute>
+                     <Signup />
+                  </PublicRoute>
+               }
+            />
+
+            <Route
+               path="/proyectos"
+               element={
+                  <PrivateRoute>
+                     <Projects />
+                  </PrivateRoute>
+               }
+            />
          </Routes>
       </Router>
    );

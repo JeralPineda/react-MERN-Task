340503e33dbd717af4ba0b4bfbde0620fcbdfc23
Jeral <jeral.cris@gmail.com>
2022-01-24 18:14:35 -0600

    Mostrando informacion del usuario autenticado

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import AuthContext from '../../context/auth/authContext';
 
 const Header = () => {
+   //Extraer el usuario del authContext
+   const authContext = useContext(AuthContext);
+   const { usuario, usuarioAutenticado } = authContext;
+
+   useEffect(() => {
+      usuarioAutenticado();
+   });
+
    return (
       <header className="app-header">
-         <p className="nombre-usuario">
-            Hola <span>Jeral Pineda</span>
-         </p>
+         {usuario ? (
+            <p className="nombre-usuario">
+               Hola <span>{usuario.name}</span>
+            </p>
+         ) : null}
 
          <nav className="nav-principal">
             <a href="#!">Cerrar Sesión</a>

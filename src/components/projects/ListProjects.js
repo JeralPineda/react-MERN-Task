32df913e5274722e47f9f4baa5ecffd44fb53d1e
Jeral <jeral.cris@gmail.com>
2022-01-25 19:44:15 -0600

    Mostrando alerta si hay error

--- src/components/projects/ListProjects.js
+++ src/components/projects/ListProjects.js
@@ -4,22 +4,37 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Project from './Project';
 
 import proyectoContext from '../../context/projects/projectContext';
+import AlertaContext from '../../context/alerts/alertContext';
 
 const ListProjects = () => {
    // obtener los proyectos desde el state inicial context
    const proyectosContext = useContext(proyectoContext);
-   const { proyectos, obtenerProyectos, nodeRef } = proyectosContext;
+   const { proyectos, mensaje, obtenerProyectos, nodeRef } = proyectosContext;
+
+   const alertaContext = useContext(AlertaContext);
+   const { alerta, mostrarAlerta } = alertaContext;
 
    // Obtener proyectos cuando carga el componente
    useEffect(() => {
+      if (mensaje) {
+         mostrarAlerta(mensaje.msg, mensaje.categoria);
+      }
+
       obtenerProyectos();
       // eslint-disable-next-line
-   }, []);
+   }, [mensaje]);
 
    if (proyectos.length === 0) return <p>No hay proyectos, comienza creando uno</p>;
 
    return (
       <ul className="listado-proyectos">
+         {alerta ? (
+            <div
+               //
+               className={`alerta ${alerta.categoria}`}>
+               {alerta.msg}
+            </div>
+         ) : null}
          <TransitionGroup>
             {proyectos.map((proyecto) => (
                <CSSTransition
